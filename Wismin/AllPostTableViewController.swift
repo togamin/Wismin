425a80016928b65c266f82e7c398106d9d45be15
Togami Yuki <togaminnnn@gmail.com>
2018-10-23 20:08:18 +0800

    セルのデザイン作成

--- Wismin/AllPostTableViewController.swift
+++ Wismin/AllPostTableViewController.swift
@@ -8,12 +8,29 @@
 
 import UIKit
 
+var defaultColor:UIColor = UIColor(red: 9/255, green: 0, blue: 54/255, alpha: 0.8)
+
 class AllPostTableViewController: UITableViewController {
 
+    var postData:[String:Any]!
+    var postDataList:[[String:Any]]!
+    
+    @IBOutlet var postTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        //サンプルデータ
+        postData = ["userImage":"Wismintest02.jpg","postDate":"2018/10/22","goodNum":"100","sendNum":"150","backImage":"backImage.jpg","wiseSaying":"You look perfect tonight.","poster":"togaminnnn"]
+        postDataList = [postData,postData,postData]
+        
+        //xibファイルの設定
+        let nib = UINib(nibName:"PostTableViewCell",bundle:nil)
+        postTableView.register(nib, forCellReuseIdentifier: "postCell")
+        postTableView.estimatedRowHeight = 250
+        postTableView.rowHeight = UITableViewAutomaticDimension//自動的にセルの高さを調節する
+        
+        //view.backgroundColor = viewColor
+        
     }
     
     override func viewDidAppear(_ animated: Bool){
@@ -34,20 +51,59 @@ class AllPostTableViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         
     }
-
-
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
-        return 0
+    
+    //セクションの数
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return postDataList.count
     }
 
+
+    //セルの数
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return 1//postDataList.count
+    }
     
+    //セルの高さ
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 369
+    }
+    // セルの上部のスペース.一番上のやつだけなしにする処理が必要。
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 5
+    }
+    // セルの下部のスペース.一番下のやつだけなしにする処理が必要。
+    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 5
+    }
+    
+
+    //セルのインスタンス化
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath) as! PostTableViewCell
 
-        // Configure the cell...
+        cell.userImage.image = UIImage(named: postDataList[indexPath.section]["userImage"] as! String)
+        cell.postDate.text = postDataList[indexPath.section]["postDate"] as! String
+        cell.goodNum.text = postDataList[indexPath.section]["goodNum"] as! String
+        cell.sendNum.text = postDataList[indexPath.section]["sendNum"] as! String
+        cell.backImage.image = UIImage(named: postDataList[indexPath.section]["backImage"] as! String)
+        cell.wiseSaying.text = postDataList[indexPath.section]["wiseSaying"] as! String
+        
+        var posterStr = postDataList[indexPath.section]["poster"] as! String
+        cell.poster.text = "by" + " " + posterStr
+        
+        //セルのレイアウト
+        cell.layer.cornerRadius = 15
+        cell.layer.borderWidth = 1
+        cell.layer.borderColor = defaultColor.cgColor
+        // 影の設定
+        cell.layer.shadowOpacity = 0.6
+        cell.layer.shadowRadius = 9
+        cell.layer.shadowColor = UIColor.black.cgColor
+        cell.layer.shadowOffset = CGSize(width: 3, height: 3)
+        
 
+        
         return cell
     }
 }
--- Wismin/PostTableViewCell.swift
+++ Wismin/PostTableViewCell.swift
@@ -15,12 +15,16 @@ class PostTableViewCell: UITableViewCell {
     @IBOutlet weak var userImage: UIImageView!
     @IBOutlet weak var postDate: UILabel!
     @IBOutlet weak var goodNum: UILabel!
+    @IBOutlet weak var sendNum: UILabel!
+    
     
     @IBOutlet weak var backImage: UIImageView!
     @IBOutlet weak var wiseSaying: UILabel!
     @IBOutlet weak var poster: UILabel!
     
-    
+    @IBOutlet weak var good: UIButton!
+    @IBOutlet weak var send: UIButton!
+    @IBOutlet weak var share: UIButton!
     
     
     @IBAction func inspireBtn(_ sender: UIButton) {
@@ -34,16 +38,64 @@ class PostTableViewCell: UITableViewCell {
     
     @IBAction func goodBtn(_ sender: UIButton) {
         //いいねの画像を変更し、数字を増やす。いいねリストに追加する。
-        print("いいねする")
+        print("memo:いいねする")
     }
     
     
-    
+    //セルが読み込まれた後?に動作する。
     override func awakeFromNib() {
         super.awakeFromNib()
         print("memo:awakeFromNib関数")
+        
+        let screenWidth = UIScreen.main.bounds.size.width
+        let btnsize = screenWidth * 0.08
+        
+        //背景画像のレイアウト
+        backImage.frame = CGRect(x: 0, y: 0, width: screenWidth, height: self.frame.size.width)
+        backImage.contentMode = UIViewContentMode.scaleAspectFill
+        backImage.layer.cornerRadius = 15
+        backImage.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        
+        //ユーザー画像
+        userImage.frame = CGRect(x: 9, y: backImage.frame.size.height - userImage.frame.size.height/2, width: self.frame.size.width * 0.2, height: self.frame.size.width * 0.2)
+        userImage.layer.cornerRadius = userImage.frame.size.width * 0.5
+        self.addSubview(userImage)
+        
+        //postDate
+        postDate.frame = CGRect(x: screenWidth - 129, y: 3, width: 120, height: 15)
+        self.addSubview(postDate)
+        
+        //wiseSaying
+        wiseSaying.center = backImage.center
+        
+        //poster
+        poster.frame = CGRect(x: self.frame.size.width * 0.2 + 18, y:self.frame.size.width - 36, width: screenWidth - (self.frame.size.width * 0.2 + 27), height: 30)
+        
+        //Good画像について
+        let goodX = 18 + userImage.frame.size.width
+        good.frame = CGRect(x: goodX, y: backImage.frame.size.height + 6, width: btnsize, height: btnsize)
+        self.addSubview(good)
+        
+        
+        //Goodの数字
+        goodNum.frame = CGRect(x: Int(18 + userImage.frame.size.width + btnsize + 3), y: 369 - 27, width: 50, height: 15)
+        self.addSubview(goodNum)
+        
+        //Send画像について
+        send.frame = CGRect(x: screenWidth/2, y: backImage.frame.size.height + 6, width: btnsize, height: btnsize)
+        self.addSubview(send)
+        
+        //Sendの数字
+        sendNum.frame = CGRect(x: Int(screenWidth/2 + btnsize + 3), y: 369 - 27, width: 50, height: 15)
+        
+        //Share画像について
+        share.frame = CGRect(x: screenWidth - screenWidth * 0.08 - 9, y: backImage.frame.size.height + 6, width: btnsize, height: btnsize)
+        self.addSubview(share)
+        
+        
     }
 
+    //画面上にセルが押されるたびに動作する。
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         print("memo:setSelected関数")
